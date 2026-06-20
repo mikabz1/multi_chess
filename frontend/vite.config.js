30e3893335b7c402f7860c1ba9775bdfb6a8cdf3
Michael Benzekri <mikabz1@gmail.com>
2026-06-20 13:44:13 +0300

    add allowedHosts to vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,9 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
+    allowedHosts: [
+      'multi-chess.gleeze.com'
+    ],
     proxy: {
       '/api': {
         target: 'http://backend:5000',
